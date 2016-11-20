4e1f15bdadb55cbb4f45cf8814f00c3a92a8323f
Timwi <github@timwi.de>
2016-11-20 16:13:23 +0100

    Unfinished

---
+++ Assets/HexamazeModule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Hexamaze;
+using UnityEngine;
+using Rnd = UnityEngine.Random;
+
+/// <summary>
+/// On the Subject of Hexamazes
+/// Created by Timwi
+/// </summary>
+public class HexamazeModule : MonoBehaviour
+{
+    public KMBombInfo Bomb;
+    public KMBombModule Module;
+    public KMAudio Audio;
+
+    void Start()
+    {
+        Debug.Log("[Hexamaze] Started");
+        Module.OnActivate += ActivateModule;
+    }
+
+    void ActivateModule()
+    {
+    }
+}
